10fb2d8820fc9e6573469d90872195513d35fc64
Varunraj Rachakatla <s556300@nwmissouri.edu>
2023-10-25 10:34:53 -0500

    CSIS - 44563 - A09 - Fourth commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var sum = 0;
+var more = 0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more++;
+  sum = more;
+  res.send(`Sum is: ${sum}`);
 });
 
 module.exports = router;
